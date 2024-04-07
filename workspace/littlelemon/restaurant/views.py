8b5f8d1c1bf63e0ab6ab27c1f0f3079c3ff3dec5
Pan Gong <pn819328@dal.ca>
2024-04-07 17:00:58 -0300

    Add static pages

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import datetime  # Import the datetime module
 
 # Create your views here.
+from django.http import HttpResponse
+
+# def sayHello(request):
+#     return HttpResponse('Hello World')
+def index(request):
+    current_year = datetime.datetime.now().year  # Get the current year
+    return render(request, 'index.html', {'current_year': current_year})
