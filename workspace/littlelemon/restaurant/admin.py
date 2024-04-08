dd296bcf95901312211a0b3fc099850246e827d6
Pan Gong <pn819328@dal.ca>
2024-04-07 23:46:56 -0300

    set menu and book models

--- workspace/littlelemon/restaurant/admin.py
+++ workspace/littlelemon/restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Menu, Booking  # Import the models from the models.py file in the same directory
 
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Booking)
--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.Title
+
+class Booking(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.Name} - {self.BookingDate.strftime('%Y-%m-%d %H:%M')}"
